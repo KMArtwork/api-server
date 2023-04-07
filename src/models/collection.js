eca0d0f69c6f870d2caa2e6785b17513890ff0ac
KMART <93807261+KMArtwork@users.noreply.github.com>
2023-04-06 17:13:06 -0700

    update collection CRUD operations

--- src/models/collection.js
+++ src/models/collection.js
@@ -2,39 +2,51 @@
 
 class Collection {
   constructor(dbModel) {
+    // dbModel is a reference to a database model that has been instantiated with sequelize
     this.dbModel = dbModel;
   }
 
   create = async (modelData)  => {
     try {
-      this.dbModel.create(modelData)
+      // .create() is a method of sequelize that combines the methods .build() and .save() into one
+      return await this.dbModel.create(modelData)
     }
     catch (error) {
       console.error('Cannot Create DB Entry', error)
     }
   }
 
-  read = async () => {
+  read = async (id, findOptions) => {
     try {
-
+      if (id) {
+        return await this.dbModel.findOne({where: {id}, ...findOptions})
+      }
+      else {
+        return await this.dbModel.findAll(findOptions)
+      }
     }
     catch (error) {
       console.error('Cannot Read DB Entry', error)
     }
   }
 
-  update = async () => {
+  update = async (id, modelData) => {
     try {
-
+      return await this.model.update(
+        modelData,
+        { where: {id}}
+      )
     }
     catch (error) {
       console.error('Cannot Update DB Entry', error)
     }
   }
 
-  delete  = async () => {
+  delete  = async (id) => {
     try {
-
+      return await this.dbModel.destroy({
+        where: {id}
+      })
     }
     catch (error) {
       console.error('Cannot Delete DB Entry', error)
